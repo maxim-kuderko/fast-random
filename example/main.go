a1f41c52f02dd7f8a94ea061daafa8ad8af31747
Max Kuderko <kuderko@gmail.com>
2021-06-10 22:33:31 +0300

    better example

--- example/main.go
+++ example/main.go
@@ -3,20 +3,20 @@ package main
 import (
 	fast_random "github.com/maxim-kuderko/fast-random"
 	"math/rand"
-	"runtime"
 	"sync"
 	"time"
 )
 
 func main() {
-	source := fast_random.NewSource(8, func() int64 {
+	concurrency := 16
+	source := fast_random.NewSource(concurrency, func() int64 {
 		return time.Now().UnixNano()
 	})
 	r := rand.New(source)
 
 	wg := sync.WaitGroup{}
-	wg.Add(runtime.GOMAXPROCS(0))
-	for i := 0; i < 16; i++ {
+	wg.Add(concurrency)
+	for i := 0; i < concurrency; i++ {
 		go func() {
 			defer wg.Done()
 			for j := 0; j < 100000000; j++ {
