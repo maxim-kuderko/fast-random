72ecc8d44383cbc97581030722e84e48a61b509c
Maxim Kudekro <7356439+maxim-kuderko@users.noreply.github.com>
2021-06-12 03:20:18 +0300

    + add helper rand function

--- example/main.go
+++ example/main.go
@@ -1,26 +1,19 @@
 package main
 
 import (
-	fast_random "github.com/maxim-kuderko/fast-random"
-	"math/rand"
+	rand "github.com/maxim-kuderko/fast-random"
 	"sync"
-	"time"
 )
 
 func main() {
 	concurrency := 16
-	source := fast_random.NewSource(concurrency, func() int64 {
-		return time.Now().UnixNano()
-	})
-	r := rand.New(source)
-
 	wg := sync.WaitGroup{}
 	wg.Add(concurrency)
 	for i := 0; i < concurrency; i++ {
 		go func() {
 			defer wg.Done()
 			for j := 0; j < 100000000; j++ {
-				r.Int63()
+				rand.Int63()
 			}
 		}()
 	}
