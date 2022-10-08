610c7a7ff20f94043b5e4453c1db5825df478997
Coby Benveniste <coby@base.la>
2022-10-08 17:45:14 +0300

    Combine to one source

--- bench_test.go
+++ bench_test.go
@@ -8,24 +8,9 @@ import (
 var x int64
 
 // BenchmarkModulo-10    	68138263	        17.57 ns/op	       0 B/op	       0 allocs/op
-func BenchmarkModulo(b *testing.B) {
+// BenchmarkSource-10    	77507067	        15.56 ns/op	       0 B/op	       0 allocs/op
+func BenchmarkSource(b *testing.B) {
 	src := NewSource(64, func() int64 {
-		return time.Now().UnixNano()
-	})
-
-	b.ResetTimer()
-	b.ReportAllocs()
-	var n int64
-	for i := 0; i < b.N; i++ {
-		n = src.Int63()
-	}
-	b.StopTimer()
-	x = n
-}
-
-// BenchmarkBitShift-10    	73486071	        15.76 ns/op	       0 B/op	       0 allocs/op
-func BenchmarkBitShift(b *testing.B) {
-	src := NewBitShiftSource(64, func() int64 {
 		return time.Now().UnixNano()
 	})
 
--- source.go
+++ source.go
@@ -1,34 +1,47 @@
 package fast_random
 
 import (
+	"math/bits"
 	"math/rand"
 	"sync"
 	"sync/atomic"
+
+	"github.com/probably-not/pow"
 )
 
 type Source struct {
 	orig   []rand.Source
 	shards uint32
+	mask   uint32
 
 	atomic32 *uint32
 	lock     []sync.Mutex
 }
 
 func NewSource(shards uint32, seedFn func() int64) *Source {
-	sources := make([]rand.Source, 0, shards)
-	for i := uint32(0); i < shards; i++ {
+	closestPow := uint32(pow.ClosestPowerOfTwoBitwise(int64(shards)))
+	factor := uint32(bits.TrailingZeros32(closestPow))
+
+	sources := make([]rand.Source, 0, closestPow)
+	for i := uint32(0); i < closestPow; i++ {
 		sources = append(sources, rand.NewSource(seedFn()))
 	}
+
 	i := uint32(0)
-	return &Source{orig: sources, atomic32: &i, shards: shards, lock: make([]sync.Mutex, shards)}
+	return &Source{
+		orig:     sources,
+		atomic32: &i,
+		shards:   closestPow,
+		mask:     (uint32(1) << uint32(factor)) - 1,
+		lock:     make([]sync.Mutex, closestPow),
+	}
 }
 
 func (s *Source) Int63() int64 {
 	n := atomic.AddUint32(s.atomic32, 1)
-	if n >= s.shards {
-		atomic.CompareAndSwapUint32(s.atomic32, n, 0)
-	}
-	shard := n % s.shards
+	head := n & s.mask
+	shard := (head + uint32(1)) & s.mask
+
 	s.lock[shard].Lock()
 	defer s.lock[shard].Unlock()
 	return s.orig[shard].Int63()
